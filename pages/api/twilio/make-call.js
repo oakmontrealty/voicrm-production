be4fa5ab374b7ff1b02f61916492745540a5159f
oakmontrealty <terence@oakmontrealty.com.au>
2025-08-12 16:57:06 +1000

    Fix: Remove ALL TypeScript files, use JavaScript only

--- pages/api/twilio/make-call.js
+++
@@ -1,4 +0,0 @@
-// Temporarily disabled - complex dependencies
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Twilio integration coming soon' })
-}
--- pages/api/voice/transcribe.js
+++
@@ -1,4 +0,0 @@
-// Temporarily disabled - complex dependencies
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Voice transcription coming soon' })
-}
